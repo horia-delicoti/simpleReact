35a66df4aec363504443b30ac838d895e5d7927e
Horia Delicoti <horia@balena.io>
2019-07-17 13:13:36 +0300

    Declaring a winner
    Signed-off-by: Horia Delicoti <horia@balena.io>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -47,6 +47,9 @@ class Board extends React.Component {
      *  - replace the date with a new copy which has the desired changes
      */
     const squares = this.state.squares.slice(); // we call ".slice()" to create a copy of the "squares" array
+    if (calculateWinner(squares) || squares[i]) {
+      return; // return early by ignoring a click if someone has won the game or if a Square is already filled
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -70,8 +73,14 @@ class Board extends React.Component {
   }
 
   render() {
-    // Displaying the "status" text which player has the next turn
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares) // checking if a player has won
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      // Displaying the "status" text which player has the next turn
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -118,3 +127,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// Given an array of 9 squares, this function will check for a winner and return 'X', 'O' or null as appropriate.
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
