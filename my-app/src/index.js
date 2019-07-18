35a50ea12a8016fa48fa165a38eb08dad225df00
Horia Delicoti <horia@balena.io>
2019-07-18 14:22:30 +0300

    Storing a history of moves * Lifting state up, again
    Signed-off-by: Horia Delicoti <horia@balena.io>

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -20,43 +20,24 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  /* We want to store the games's state in the parent Board component. The Board component can tell each Square what to
-  * display by passing a prop.
-  * The parent component can pass the state back down to the children by using "props".
-  * Adding the constructor to the Board and setting the Board's initial state to contain an array of 9 nulls
-  * corresponding to the 9 squares:
-  */
-  constructor(props) {
-    super(props);
-    /* When we fill the board in later, the "this.state.squares" array will look something like this:
-    * [   '0', null, 'X',
-    *     'X', 'X',  'X',
-    *     '0', null, null  ]
-    */
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true // setting the first move to be "X" to be default by modifying the initial state
-    }
-  }
-
-  handleClick(i) {
-    /* Why immutability is important.
-     * We use ".slice()" method to create a copy of the squares array to modify instead of modifying the existing array.
-     * There are two approaches to change data:
-     *  - mutate the data by directly changing the data's values
-     *  - replace the date with a new copy which has the desired changes
-     */
-    const squares = this.state.squares.slice(); // we call ".slice()" to create a copy of the "squares" array
-    if (calculateWinner(squares) || squares[i]) {
-      return; // return early by ignoring a click if someone has won the game or if a Square is already filled
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      // Each time a players moves, "xIsNext" will be flipped to determine which player goes next and the game's state will be saved
-      xIsNext: !this.state.xIsNext,
-    });
-  }
+  // /* We want to store the games's state in the parent Board component. The Board component can tell each Square what to
+  // * display by passing a prop.
+  // * The parent component can pass the state back down to the children by using "props".
+  // * Adding the constructor to the Board and setting the Board's initial state to contain an array of 9 nulls
+  // * corresponding to the 9 squares:
+  // */
+  // constructor(props) {
+  //   super(props);
+  //   /* When we fill the board in later, the "this.state.squares" array will look something like this:
+  //   * [   '0', null, 'X',
+  //   *     'X', 'X',  'X',
+  //   *     '0', null, null  ]
+  //   */
+  //   this.state = {
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true // setting the first move to be "X" to be default by modifying the initial state
+  //   }
+  // }
 
   renderSquare(i) {
     /* Using the prop passing mechanism, modifying the Board to instruct each individual Square about its current
@@ -67,24 +48,23 @@ class Board extends React.Component {
      * */
     return <
       Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
     />;
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares) // checking if a player has won
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      // Displaying the "status" text which player has the next turn
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+    // const winner = calculateWinner(this.state.squares) // checking if a player has won
+    // let status;
+    // if (winner) {
+    //   status = 'Winner: ' + winner;
+    // } else {
+    //   // Displaying the "status" text which player has the next turn
+    //   status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // }
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -106,14 +86,62 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  // Setting up the initial state for the Game component within its constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1]
+    /* Why immutability is important.
+     * We use ".slice()" method to create a copy of the squares array to modify instead of modifying the existing array.
+     * There are two approaches to change data:
+     *  - mutate the data by directly changing the data's values
+     *  - replace the date with a new copy which has the desired changes
+     */
+    const squares = current.squares.slice(); // we call ".slice()" to create a copy of the "squares" array
+    if (calculateWinner(squares) || squares[i]) {
+      return; // return early by ignoring a click if someone has won the game or if a Square is already filled
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      // Each time a players moves, "xIsNext" will be flipped to determine which player goes next and the game's state will be saved
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares)
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
